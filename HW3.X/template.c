495f28f0f31950e2bf059b51263c806043b64457
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2023-04-06 09:35:49 -0500

    HW3 submission
    successfully plots a sine wave of amplitude 1.65V and midpoint 1.65V

--- HW3.X/template.c
+++ HW3.X/template.c
@@ -11,15 +11,11 @@ int main(void) {
   //generate sine wave array
   float sine_array[100];
   for (int ii = 0; ii < 100; ++ii) {
-      sine_array[ii] = sin(ii * (2*3.14159/100));
+      sine_array[ii] = 1.65*sin(ii * (2*3.14159/100)) + 1.65;
   }
     
   NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
   while (1) {
-      
-//    NU32DIP_WriteUART1("Test\r\n");
-//    _CP0_SET_COUNT(0);
-//    while(_CP0_GET_COUNT() < 24000*time_ms){}
       
 	if (!NU32DIP_USER){
         for (int jj = 0; jj < 100; ++jj){
@@ -34,24 +30,3 @@ int main(void) {
 	}
   }
 }
-
-// blink the LEDs
-void blink(int iterations, int time_ms){
-	int i;
-	unsigned int t;
-	for (i=0; i< iterations; i++){
-		NU32DIP_GREEN = 0; // on
-		NU32DIP_YELLOW = 1; // off
-		t = _CP0_GET_COUNT(); // should really check for overflow here
-		// the core timer ticks at half the SYSCLK, so 24000000 times per second
-		// so each millisecond is 24000 ticks
-		// wait half in each delay
-		while(_CP0_GET_COUNT() < t + 12000*time_ms){}
-		
-		NU32DIP_GREEN = 1; // off
-		NU32DIP_YELLOW = 0; // on
-		t = _CP0_GET_COUNT(); // should really check for overflow here
-		while(_CP0_GET_COUNT() < t + 12000*time_ms){}
-	}
-}
-		

4a3c282874aed1c3f435cf72f96995b7b28d5590
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2023-04-25 11:10:09 -0500

    HW6: got I2C writes working
    had a bad line of code where I was sending a start bit instead of a stop bit

--- HW6.X/template.c
+++ HW6.X/template.c
@@ -1,15 +1,28 @@
 #include "nu32dip.h" // constants, functions for startup and UART
+#include "i2c_master_noint.h"
+#include <stdio.h>
+
+//#define FREQ_HZ 20
+#define FREQ_HZ 20
 
 #define DEVICE_ADDR 0b000
 #define OPCODE 0b0100
-#define CLIENT_ADDR (OPCODE << 4 | DEVICE_ADDR << 1)
+#define CLIENT_ADDR ((OPCODE << 4) | (DEVICE_ADDR << 1))
+
+#define DELAY_MS (float)(1000 / FREQ_HZ)
+#define DELAY_CLOCK (uint32_t)(24000 * DELAY_MS)
 
 #define READ 1
 #define WRITE 0
 
+char uart_buff[200];
+
+unsigned char read_gp0();
 void turn_on_gp7();
 void turn_off_gp7();
-uint8_t byte;
+void init_mcp23008();
+
+unsigned char byte;
 
 int main(void) {
 
@@ -19,17 +32,52 @@ int main(void) {
   i2c_master_setup();
   
   //init the mcp230008 chip
-    //write to IODIR reg, make GP7 output and GP0 input
+  init_mcp23008();
+  _CP0_SET_COUNT(0);
+  while (_CP0_GET_COUNT() < DELAY_CLOCK) {/*nothing*/}
+  NU32DIP_WriteUART1("Initializing UART\r\n");
   
   while (1) {    
             
-      
+//        _CP0_SET_COUNT(0);
+//        
+//        byte = read_gp0();
+//        sprintf(uart_buff, "Byte received: %d\r\n", byte);
+//        NU32DIP_WriteUART1(uart_buff);
+//        
+//        while (_CP0_GET_COUNT() < DELAY_CLOCK) {/*nothing*/}
+
+      NU32DIP_WriteUART1("Top of the loop\r\n");
+      _CP0_SET_COUNT(0);
       
       //blink the LED at 20Hz
-      turn_on_gp7()
+      turn_on_gp7();
+      while (_CP0_GET_COUNT() < DELAY_CLOCK) {/*nothing*/}
       //delay(), 20Hz
-      turn_off_gp7()
+      turn_off_gp7();
+      while (_CP0_GET_COUNT() < 2*DELAY_CLOCK) {/*nothing*/}
+
   }
+}
+
+void init_mcp23008() {
+    //write to IODIR reg, make GP7 output and GP0 input
+
+    //send a start bit
+    i2c_master_start();
+    
+    //send the address of the chip with a write bit
+   //0b0100 0000 
+    i2c_master_send(CLIENT_ADDR | WRITE);
+       
+    //send the reg to write to (IODIR)
+    i2c_master_send(0x00);      
+    
+    //send the value to put in that register
+    i2c_master_send((0b0 << 7) | 0b1);
+            
+    //send the stop bit
+    i2c_master_stop();  
 }
 
 void turn_on_gp7() {
@@ -41,38 +89,77 @@ void turn_on_gp7() {
     //send the address of the chip with a write bit
    //0b0100 0000 
     i2c_master_send(CLIENT_ADDR | WRITE);
-   
-    //do we have to send an ACK as the master?
-    
+       
     //send the reg to change (OLAT, to turn on a pin)
     i2c_master_send(0x0A);      
     
     //send the value to put in that register
     i2c_master_send(0b1 << 7);
             
+    //send the stop bit
+    i2c_master_stop();
     
-    //send the s
+}
+
+
+void turn_off_gp7() {
+    //sent over I2C
     
+    //send a start bit
+    i2c_master_start();
+    
+    //send the address of the chip with a write bit
+   //0b0100 0000 
+    i2c_master_send(CLIENT_ADDR | WRITE);
+       
+    //send the reg to change (OLAT, to turn on a pin)
+    i2c_master_send(0x0A);      
+    
+    //send the value to put in that register
+    i2c_master_send(0b0 << 7);
+            
+    //send the stop bit
+    i2c_master_stop();
     
 }
 
 unsigned char read_gp0(){
     
-    //send start bit
-    //send the address with the write bit
-    //send the reg you want to read from
-    //0x09
+    //send a start bit
+    i2c_master_start();
+    
+    //send the address of the chip with a write bit
+   //0b0100 0000 
+    i2c_master_send(CLIENT_ADDR | WRITE);
+       
+    //send the reg to read from (GPIO reg, posn 0))
+    i2c_master_send(0x09);      
+    
+    //send the value to put in that register
+    i2c_master_send(0b0 << 7);
+            
+    //send the stop bit
+    //i2c_master_stop();
+  
     //send restart
+    i2c_master_restart();
+    
+    //do we have to send a start bit in here?
+    
     //send the address w/ read bit
     //0b01000001
-    //unsigned char a = recv() //receive, rather than read
-    //send an ACK bit
+    i2c_master_send(CLIENT_ADDR | WRITE);
+
+    //receive, rather than read
+    unsigned char a = i2c_master_recv();
+    
+    //send an ACK bit - for reading only I think?
+    i2c_master_ack(0);
+    
     //send a stop bit
+    i2c_master_stop();
     
     //other GPIOs will also return data on this read() command;
     //need to isolate our read to only GP0
-    
-    //return (a & 0b1);
-    //if this were not GP0 we would have to do (a & 0b00000100) > 2) for GP2, e.g.
-    
+    return (a & 1);    
 }

89ede8cc0b3d51e9dbfcc83dea326df37268f69f
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2023-06-07 16:50:03 -0500

    HW14: initial commit

---
+++ HW14.X/template.c
@@ -0,0 +1,11 @@
+#include "nu32dip.h" // constants, functions for startup and UART
+
+
+int main(void) {
+  NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
+    
+  while (1) {
+
+      
+   }
+}
